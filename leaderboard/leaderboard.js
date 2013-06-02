5213de84dd85cba7d2694bf9500d98ccf03803aa
Hack Reactor Students <github@hackreactor.com>
2013-06-02 12:02:13 -0700

    subtracting points from players

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -20,6 +20,10 @@ if (Meteor.isClient) {
   Template.leaderboard.events({
     'click input.inc': function () {
       Players.update(Session.get("selected_player"), {$inc: {score: 5}});
+    },
+
+    'click input.dec': function() {
+      Players.update(Session.get("selected_player"), {$inc: {score: -4}});
     }
   });
 
